c041ae0859e61aa67989798a59e4cd0f30c19c93
nnanna <atunnannasamson@gmail.com>
2025-01-16 18:29:25 +0100

    Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Nnanna Atu <https://github.com/nnannatu>
+ */
+const displayMessage = (msg) => {
+    console.log(msg);
+  };
+  
+  module.exports = displayMessage;
